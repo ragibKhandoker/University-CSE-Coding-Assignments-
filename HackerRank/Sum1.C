29da5273c09a2654bbf3ea26a135949a050cd594
Rafi Ahsan Ragib <138756650+ragibKhandoker@users.noreply.github.com>
2024-10-27 23:55:51 +0600

    Add files via upload

---
+++ HackerRank/Sum1.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int x1;
+    printf("Enter array length:\n");
+    scanf("%d",&x1);
+    int arr[x1];
+    int i=0;
+    while(i<x1){
+        printf("Enter array elements:\n");
+        scanf("%d",&arr[i]);
+        i++;
+    }
+    printf("The array is:\n");
+    for(int j=0; j<x1; j++){
+        printf("%d ",arr[j]);
+    }
+    int k=0;
+    int sum = 0;
+    while(k<=x1){
+        sum+=k;
+        k++;
+    }
+    printf("\n%d",sum);
+    return 0;
+}
